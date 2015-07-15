8cd90097754bd6401ad43207bac556757d350ae2
Homletmoo <samlhub@gmail.com>
2015-07-16 00:56:37 +0100

    Initial commit.

---
+++ js/game.js
@@ -0,0 +1,6 @@
+/// <reference path="../../typings/pixi/pixi.d.ts"/>
+
+var container = PIXI.Container();
+var renderer = PIXI.autoDetectRenderer(640, 480);
+
+document.body.appendChild(renderer.view);
