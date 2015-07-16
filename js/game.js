d14c0eb2985c2a277498ebfa93eba8cb900d33a4
Homletmoo <samlhub@gmail.com>
2015-07-16 01:20:21 +0100

    Create an inception point for the game.

--- js/game.js
+++ js/game.js
@@ -1,6 +1,20 @@
-/// <reference path="../../typings/pixi/pixi.d.ts"/>
+/// <reference path="../typings/pixi/pixi.d.ts"/>
 
-var container = PIXI.Container();
-var renderer = PIXI.autoDetectRenderer(640, 480);
-
-document.body.appendChild(renderer.view);
+function begin()
+{
+    var container = new PIXI.Container();
+    var renderer = PIXI.autoDetectRenderer(
+        640,
+        480,
+        {view: document.getElementById('game-canvas')}
+    );
+    
+    requestAnimationFrame(update);
+    
+    function update()
+    {
+        renderer.render(container);
+        
+        requestAnimationFrame(update);
+    }
+}
