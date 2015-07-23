601417b128119c15ad33852068c71ad7493e6ebe
Homletmoo <samlhub@gmail.com>
2015-07-23 02:02:47 +0100

    Move game related variables to the Game namespace.

--- game/game.js
+++ game/game.js
@@ -1,20 +1,22 @@
 /// <reference path="../typings/pixi/pixi.d.ts"/>
 
-function begin()
+var Game = Game || {};
+
+Game.begin = function ()
 {
-    var container = new PIXI.Container();
-    var renderer = PIXI.autoDetectRenderer(
+    Game.container = new PIXI.Container();
+    Game.renderer = PIXI.autoDetectRenderer(
         800,
         600,
         {view: document.getElementById('game-canvas')}
     );
     
-    requestAnimationFrame(update);
+    requestAnimationFrame(Game.update);
+}
+
+Game.update = function ()
+{
+    Game.renderer.render(Game.container);
     
-    function update()
-    {
-        renderer.render(container);
-        
-        requestAnimationFrame(update);
-    }
+    requestAnimationFrame(Game.update);
 }
