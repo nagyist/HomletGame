bd98162c60b1846ac7ad0bdcd84b20f9d6936006
Homletmoo <samlhub@gmail.com>
2015-08-09 23:51:15 +0100

    Implement Addable in State.ts.

--- game/HG/State.ts
+++ game/HG/State.ts
@@ -1,20 +1,26 @@
 /// <reference path="../typings/pixi/pixi.d.ts"/>
+/// <reference path="Interface/Addable.ts"/>
 /// <reference path="Interface/Steppable.ts"/>
 
 module HG
 {
-    export class State extends PIXI.Container implements Interface.Steppable
+    export class State extends PIXI.Container implements Interface.Steppable, Interface.Addable
     {
         /**
          * Called when the state is loaded in the game.
          */
-        load() {};
+        added() : void { /* Abstract method. */ };
         
         /**
          * Called once per game logic loop.
          */
-        step(delta : number) {
+        step(delta : number) : void {
             // TODO: step entities individually.
         }
+        
+        /**
+         * Called when the state is removed from the game.
+         */
+        removed() : void { /* Abstract method. */ };
     }
 }
