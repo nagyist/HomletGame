85e0edd05ccb6a45340163cfe52ddef1369e51eb
Homletmoo <samlhub@gmail.com>
2015-08-10 01:59:30 +0100

    Implement Container interface in State.ts.

--- game/HG/State.ts
+++ game/HG/State.ts
@@ -7,8 +7,23 @@
 module HG
 {
     export class State extends PIXI.Container
-    implements Interface.Steppable, Interface.Addable, Interface.Drawer
+    implements Interface.Steppable, Interface.Addable, Interface.Container, Interface.Drawer
     {
+        /**
+         * Array to hold entities currently added to the state.
+         */
+        private entities : Array<Entity>;
+        
+        
+        /**
+         * Initialise data members.
+         */
+        constructor() {
+            super();
+            
+            this.entities = new Array<Entity>();
+        }
+        
         /**
          * Called when the state is loaded in the game.
          */
@@ -27,8 +42,40 @@ module HG
         removed() : void { /* Abstract method. */ };
         
         /**
+         * Add an entity to be stepped and rendered while the state is loaded.
+         * Throw an error if the entity is already added.
+         */
+        add(subject : Entity) : void {
+            if (this.contains(subject)) {
+                throw Error("Attempted to add entity already added to state.")
+            } else {
+                this.entities.push(subject);
+            }
+        }
+        
+        /**
+         * Remove an entity from the state. Throw an error if the entity is
+         * not added to the state.
+         */
+        remove(subject : Entity) : void {
+            if (!this.contains(subject)) {
+                throw Error("Attempted to remove an entity not added to state.")
+            } else {
+                var index = this.entities.indexOf(subject);
+                this.entities.splice(index, 1);
+            }
+        }
+             
+        /**
+         * Return whether the state already contains an entity.
+         */
+        contains(subject : Entity) : boolean {
+            return (this.entities.indexOf(subject) > -1);
+        }
+        
+        /**
          * Called to notify the change of the DisplayObject associated with
-         * the given Entity. Throws an error if the entity is not added to
+         * the given entity. Throws an error if the entity is not added to
          * the state already. 
          */
         notifyDisplayObjectChanged(subject : Entity) : void {
