286df3b1aa509f21ec6adf6115d7e219694a5418
Homletmoo <samlhub@gmail.com>
2015-08-10 01:05:49 +0100

    Create an Entity class.

---
+++ game/HG/Entity.ts
@@ -0,0 +1,25 @@
+/// <reference path="../typings/pixi/pixi.d.ts"/>
+/// <reference path="Interface/Addable.ts"/>
+/// <reference path="Interface/Steppable.ts"/>
+
+module HG
+{
+    export class Entity implements Interface.Steppable, Interface.Addable
+    {
+        /**
+         * Called when the entity is added to a state.
+         */
+        added() : void { /* Abstract method. */ };
+        
+        /**
+         * Called once per game logic loop.
+         */
+        step(delta : number) : void { /* Abstract method. */ }
+        
+        /**
+         * Called when the entity is removed from the state,
+         * or when the state is unloaded from the game.
+         */
+        removed() : void { /* Abstract method. */ };
+    }
+}
