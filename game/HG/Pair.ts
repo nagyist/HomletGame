d21f00aa4d33804ffdb3faa2654d0d3688b715b9
Homletmoo <samlhub@gmail.com>
2015-08-20 12:21:28 +0100

    Add x and y properties to point class.

--- game/HG/Pair.ts
+++ game/HG/Pair.ts
@@ -3,4 +3,21 @@ class Pair<A, B>
     constructor(public first : A, public second : B) { }
 }
 
-class Point extends Pair<number, number> { }
+class Point extends Pair<number, number>
+{
+    get x() : number {
+        return this.first;
+    }
+    
+    set x(value : number) {
+        this.first = value;
+    }
+    
+    get y() : number {
+        return this.second;
+    }
+    
+    set y(value : number) {
+        this.second = value;
+    }
+}
