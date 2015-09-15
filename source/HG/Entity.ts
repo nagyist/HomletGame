0a691caf931305f9028382bbadbd91fa44516dfa
Homletmoo <samlhub@gmail.com>
2015-09-15 22:04:57 +0100

    Remove Drawable interface in favour of pixi containers.

--- source/HG/Entity.ts
+++ source/HG/Entity.ts
@@ -5,19 +5,13 @@
 
 module HG
 {
-    export class Entity
-    implements Interface.Steppable, Interface.Addable, Interface.Drawable
+    export class Entity extends PIXI.Container
+    implements Interface.Steppable, Interface.Addable
     {
-        /**
-         * PIXI DisplayObject associated with this Entity for drawing purposes.
-         */
-        private displayObject : PIXI.DisplayObject;
-        
-        
         /**
          * Called when the entity is added to a state.
          */
-        added() : void { /* Abstract method. */ };
+        added() : void { /* Abstract method. */ }
         
         /**
          * Called once per game logic loop.
@@ -28,64 +22,20 @@ module HG
          * Called when the entity is removed from the state,
          * or when the state is unloaded from the game.
          */
-        removed() : void { /* Abstract method. */ };
+        removed() : void { /* Abstract method. */ }
         
         /**
-         * Return the PIXI DisplayObject associated with this Entity
-         * for drawing purposes.
+         * Return a reference to the display object used for rendering. 
          */
-        getDisplayObject() : PIXI.DisplayObject {
-            return this.displayObject;
+        get displayObject() : PIXI.DisplayObject {
+            return this.getChildAt(0);
         }
         
         /**
-         * Set the PIXI DisplayObject associated with this Entity
-         * for drawing purposes.
+         * Set the display object to use for rendering.
          */
-        protected setDisplayObject(value : PIXI.DisplayObject) {
-            this.displayObject = value;
-        }
-        
-        /**
-         * Return the x coordinate of the DisplayObject. If there
-         * is no DisplayObject assigned, this defaults to zero.
-         */
-        public get x() : number {
-            if (this.displayObject != null) {
-                return this.displayObject.x;
-            } else {
-                return 0;
-            }
-        }
-        
-        /**
-         * Set the x coordinate of the DisplayObject (if one is assigned).
-         */
-        public set x(value : number) {
-            if (this.displayObject != null) {
-                this.displayObject.x = value;
-            }
-        }
-        
-        /**
-         * Return the y coordinate of the DisplayObject. If there
-         * is no DisplayObject assigned, this defaults to zero.
-         */
-        public get y() : number {
-            if (this.displayObject != null) {
-                return this.displayObject.y;
-            } else {
-                return 0;
-            }
-        }
-        
-        /**
-         * Set the y coordinate of the DisplayObject (if one is assigned).
-         */
-        public set y(value : number) {
-            if (this.displayObject != null) {
-                this.displayObject.y = value;
-            }
+        set displayObject(value : PIXI.DisplayObject) {
+            this.addChildAt(value, 0);
         }
     }
 }
--- source/HG/State.ts
+++ source/HG/State.ts
@@ -9,13 +9,8 @@
 module HG
 {
     export class State extends PIXI.Container
-    implements Interface.Steppable, Interface.Addable, Interface.Container, Interface.Drawer
-    {
-        /**
-         * Array to hold entities and display objects currently added to the state.
-         */
-        private entities : Array<Pair<Entity, PIXI.DisplayObject> >;
-        
+    implements Interface.Steppable, Interface.Addable
+    {        
         /**
          * A reference to the game object which contains the state. 
          */
@@ -29,8 +24,6 @@ module HG
             super();
             
             this.game = null;
-            
-            this.entities = new Array<Pair<Entity, PIXI.DisplayObject> >();
         }
         
         /**
@@ -42,8 +35,11 @@ module HG
          * Called once per game logic loop.
          */
         step(delta : number) : void {
-            for (var i = 0; i < this.entities.length; i++) {
-                (<Entity> this.entities[i].first).step(delta);
+            for (var i = 0; i < this.children.length; i++) {
+                // Only proper entities can be stepped.
+                if (this.children[i] instanceof Entity) {
+                    (<Entity> this.children[i]).step(delta);
+                }
             }
         }
         
@@ -51,68 +47,5 @@ module HG
          * Called when the state is removed from the game.
          */
         removed() : void { /* Abstract method. */ };
-        
-        /**
-         * Add an entity to be stepped and rendered while the state is loaded.
-         * Throw an error if the entity is already added.
-         */
-        add(subject : Entity) : void {
-            if (this.indexOf(subject) > -1) {
-                throw Error("Attempted to add entity already added to state.")
-            } else {
-                var displayObject = subject.getDisplayObject();
-                this.addChild(displayObject);
-                
-                var pair = new Pair<Entity, PIXI.DisplayObject>(subject, displayObject);
-                this.entities.push(pair);
-            }
-        }
-        
-        /**
-         * Remove an entity from the state. Throw an error if the entity is
-         * not added to the state.
-         */
-        remove(subject : Entity) : void {
-            if (this.indexOf(subject) < 0) {
-                throw Error("Attempted to remove an entity not added to state.")
-            } else {
-                var index : number = this.indexOf(subject);
-                this.removeChild(this.entities[index].second);
-                this.entities.splice(index, 1);
-            }
-        }
-             
-        /**
-         * Return the index of the entity in the state.
-         */
-        indexOf(subject : Entity) : number {
-            var index : number = -1;
-            for (var pair in this.entities) {
-                index++;
-                if (pair.first == subject) {
-                    return index
-                }
-            }
-            return -1;
-        }
-        
-        /**
-         * Called to notify the change of the DisplayObject associated with
-         * the given entity. Throws an error if the entity is not added to
-         * the state already. 
-         */
-        notifyDisplayObjectChanged(subject : Entity) : void {
-            if (this.indexOf(subject) < 0) {
-                throw Error("Cannot update display object for an entity not added to state.")
-            } else {
-                var index : number = this.indexOf(subject);
-                // Remove the old display object from the PIXI container.
-                this.removeChild(this.entities[index].second);
-                
-                var displayObject : PIXI.DisplayObject = subject.getDisplayObject();
-                this.addChild(displayObject);
-                this.entities[index].second = displayObject;
-            }
-        }
     }
 }
